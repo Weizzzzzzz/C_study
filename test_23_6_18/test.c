b9b5f6986404a46e61db08334cb82a3ecde82d3a
Weizzzzzzz <2141618527@qq.com>
2023-06-18 21:23:30 +0800

    main

---
+++ test_23_6_18/test.c
@@ -0,0 +1,88 @@
+// #include <stdio.h>
+// 
+// int main()
+// {
+//     printf("Hello Nowcoder!");
+// 
+//     return 0;
+// }
+
+// #include <stdio.h>
+// 
+// int main()
+// {
+//     printf("     **     \n");
+//     printf("     **     \n");
+//     printf("************\n");
+//     printf("************\n");
+//     printf("    *  *    \n");
+//     printf("    *  *    \n");
+// 
+//     return 0;
+// }
+
+// #include <stdio.h>
+// 
+// int main()
+// {
+//     int i = 0;
+//     scanf("%d", &i);
+//     printf("%d\n", i);
+// 
+//     return 0;
+// }
+
+// #include <stdio.h>
+// 
+// int main()
+// {
+//     float i = 0;
+//     scanf("%f", &i);
+//     printf("%.3f\n", i);
+// 
+//     return 0;
+// }
+
+// #include <stdio.h>
+// 
+// int main()
+// {
+//     char i = 0;
+//     scanf("%c", &i);
+//     printf("%c\n", i);
+// 
+//     return 0;
+// }
+
+// #include <stdio.h>
+// 
+// int main()
+// {
+//     int a = 0;
+//     int b = 0;
+//     int c = 0;
+//     scanf("%d %d %d", &a, &b, &c);
+//     printf("%d", b);
+// 
+//     return 0;
+// }
+
+// #include <stdio.h>
+// 
+// int main()
+// {
+//     char a = 0;
+//     scanf("%c", &a);
+//     int i = 0;
+//     for (i = 0; i < 3; i++)
+//     {
+//         int j = 0;
+//         for (j = 0; j < 3; j++)
+//         {
+//             printf("%c", a);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
