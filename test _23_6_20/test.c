7512a91e94d827ebaefa22c85440ab685a458561
Weizzzzzzz <2141618527@qq.com>
2023-06-21 23:43:22 +0800

    main

--- test _23_6_20/test.c
+++ test _23_6_20/test.c
@@ -1,13 +1,23 @@
+// #include <stdio.h>
+
+// int main() {
+//     char i = 0;
+//     scanf("%c",&i);
+//     printf("  %c  \n",i);
+//     printf(" %c%c%c \n",i,i,i);
+//     printf("%c%c%c%c%c\n",i,i,i,i,i);
+//     printf(" %c%c%c \n",i,i,i);
+//     printf("  %c  \n",i);
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main() {
     char i = 0;
     scanf("%c",&i);
-    printf("  %c  \n",i);
-    printf(" %c%c%c \n",i,i,i);
-    printf("%c%c%c%c%c\n",i,i,i,i,i);
-    printf(" %c%c%c \n",i,i,i);
-    printf("  %c  \n",i);
+    printf("%d\n",i);
 
     return 0;
 }
