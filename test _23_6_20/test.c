4de3770c78b4df1a8be77fafa0b8d078638b35b0
Weizzzzzzz <2141618527@qq.com>
2023-06-22 22:19:25 +0800

    main

--- test _23_6_20/test.c
+++ test _23_6_20/test.c
@@ -12,12 +12,46 @@
 //     return 0;
 // }
 
+// #include <stdio.h>
+
+// int main() {
+//     char i = 0;
+//     scanf("%c",&i);
+//     printf("%d\n",i);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     float i = 0;
+//     scanf("%f", &i);
+//     printf("%0.0f", i);
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int x = 0;
+//     int y = 0;
+//     int z = 0;
+//     scanf("%d %d %d", &x, &y, &z);
+//     printf("score1=%d,score2=%d,score3=%d", x, y, z);
+//     return 0;
+// }
+
 #include <stdio.h>
 
-int main() {
-    char i = 0;
-    scanf("%c",&i);
-    printf("%d\n",i);
-
+int main()
+{
+    int id = 0;
+    float c = 0;
+    float math = 0;
+    float english = 0;
+    scanf("%d;%f,%f,%f", &id, &c, &math, &english);
+    printf("The each subject score of No. %d is %.2f, %.2f, %.2f.", id, c, math, english);
     return 0;
 }
