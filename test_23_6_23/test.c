6b051640d28f79536d7d4db416d814cd1672c863
Weizzzzzzz <2141618527@qq.com>
2023-06-23 22:14:57 +0800

    main

---
+++ test_23_6_23/test.c
@@ -0,0 +1,13 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int y = 0;
+//     int m = 0;
+//     int d = 0;
+//     scanf("%4d%2d%2d", &y, &m, &d);
+//     printf("year=%d\nmonth=%02d\ndate=%02d\n", y, m, d);
+
+//     return 0;
+// }
+
