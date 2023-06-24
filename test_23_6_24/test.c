f7a5affb4a57699d86c69dbb65e80a0a6ebd6e45
Weizzzzzzz <2141618527@qq.com>
2023-06-24 22:56:17 +0800

    main

---
+++ test_23_6_24/test.c
@@ -0,0 +1,112 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 1;
+//     while(i<=10)
+//     {
+//         printf("%d ", i);
+//         i++; // i=i+1;
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 1;
+//     while(i<=10)
+//     {
+//         if(i == 5)
+//             break;
+//         printf("%d ", i);
+//         i++;
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 1;
+//     while (i <= 10)
+//     {
+//         if (i == 5)
+//             continue;
+//         printf("%d ", i);
+//         i++;
+//     }
+//     return 0;
+// }
+
+#include <stdio.h>
+
+// int main()
+// {
+//     int i = 0;
+//     for (i = 1; i <= 10; i++)
+//     {
+//         printf("%d ", i);
+//     }
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 1;
+//     do
+//     {
+//         printf("%d ", i);
+//         i = i + 1;
+//     } while (i <= 10);
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 1;
+//     do
+//     {
+//         if (5 == i)
+//             break;
+//         printf("%d ", i);
+//         i = i + 1;
+//     } while (i <= 10);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 1;
+
+//     do
+//     {
+//         if (5 == i)
+//             continue;
+//         printf("%d ", i);
+//         i = i + 1;
+//     } while (i <= 10);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a = 0;
+//     int b = 0;
+//     scanf("a=%d,b=%d", &a, &b);
+//     int tmp = a;
+//     a = b;
+//     b = tmp;
+//     printf("a=%d,b=%d\n", a, b);
+
+//     return 0;
+// }
+
