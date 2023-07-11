613673bb886a829237603417c997e5b4e3cf2f6f
Weizzzzzzz <2141618527@qq.com>
2023-07-11 21:11:06 +0800

    main

---
+++ test_23_7_11/test.c
@@ -0,0 +1,67 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int grade = 0;
+
+//     while ((scanf("%d", &grade)) != EOF)
+//     {
+//         if (grade >= 60)
+//         {
+//             printf("Pass\n");
+//         }
+//         else
+//         {
+//             printf("Fail\n");
+//         }
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 0;
+//     while ((scanf("%d", &i)) != EOF)
+//     {
+//         if (i % 2 == 0)
+//         {
+//             printf("Even\n");
+//         }
+//         else
+//         {
+//             printf("Odd\n");
+//         }
+//     }
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     char ch = 0;
+//     char arr[10] = {'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'};
+//     while ((scanf("%c", &ch)) != EOF)
+//     {
+//         if (ch != '\n')
+//         {
+//             int i = 0;
+//             for (i = 0; i < 10; i++)
+//             {
+//                 if (ch == arr[i])
+//                 {
+//                     printf("Vowel\n");
+//                     break;
+//                 }
+//             }
+//             if (i == 10)
+//             {
+//                 printf("Consonant\n");
+//             }
+//         }
+//     }
+//     return 0;
+// }
