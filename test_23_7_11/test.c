82900582b0b7971c1d65ac836df5b81b5115db4f
Weizzzzzzz <2141618527@qq.com>
2023-07-12 20:16:00 +0800

    main

--- test_23_7_11/test.c
+++ test_23_7_11/test.c
@@ -65,3 +65,21 @@
 //     }
 //     return 0;
 // }
+
+#include <stdio.h>
+
+int main()
+{
+    int x, l, r;
+    scanf("%d %d %d", &x, &l, &r);
+    if (x >= l && x <= r)
+    {
+        printf("true\n");
+    }
+    else
+    {
+        printf("false\n");
+    }
+
+    return 0;
+}
