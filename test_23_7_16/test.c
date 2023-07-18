5ff150de2e1eeac380a6aa87a7683917feacc873
Weizzzzzzz <2141618527@qq.com>
2023-07-18 19:42:37 +0800

    main

--- test_23_7_16/test.c
+++ test_23_7_16/test.c
@@ -47,28 +47,74 @@
 //     return 0;
 // }
 
+// #include <stdio.h>
+
+// int main()
+// {
+//     int arr[10];
+//     int i = 0;
+//     int positive = 0;
+//     int negative = 0;
+//     for (i = 0; i < 10; i++)
+//     {
+//         scanf("%d", &arr[i]);
+//         if (arr[i] > 0)
+//         {
+//             positive++;
+//         }
+//         else
+//         {
+//             negative++;
+//         }
+//     }
+//     printf("positive:%d\n", positive);
+//     printf("negative:%d\n", negative);
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
 {
-    int arr[10];
-    int i = 0;
-    int positive = 0;
-    int negative = 0;
-    for (i = 0; i < 10; i++)
+    double price = 0.0;
+    int month = 0;
+    int day = 0;
+    int flag = 0;
+    // 输入
+    scanf("%lf %d %d %d", &price, &month, &day, &flag);
+    // 计算
+    if (flag == 1)
     {
-        scanf("%d", &arr[i]);
-        if (arr[i] > 0)
+        if ((month == 11) && (day == 11))
         {
-            positive++;
+            price = price * 0.7 - 50;
         }
-        else
+        if ((month == 12) && (day == 12))
         {
-            negative++;
+            price = price * 0.8 - 50;
         }
     }
-    printf("positive:%d\n", positive);
-    printf("negative:%d\n", negative);
+    if (flag == 0)
+    {
+        if ((month == 11) && (day == 11))
+        {
+            price = price * 0.7;
+        }
+        if ((month == 12) && (day == 12))
+        {
+            price = price * 0.8;
+        }
+    }
+    // 输出
+    if (price <= 0)
+    {
+        printf("%.2lf", price = 0);
+    }
+    else
+    {
+        printf("%.2lf", price);
+    }
 
     return 0;
 }
