1842811a97495daf52e0ccd155c5b5741baa0893
Weizzzzzzz <2141618527@qq.com>
2023-07-19 19:32:00 +0800

    main

--- test_23_7_16/test.c
+++ test_23_7_16/test.c
@@ -73,48 +73,103 @@
 //     return 0;
 // }
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main()
-{
-    double price = 0.0;
-    int month = 0;
-    int day = 0;
-    int flag = 0;
-    // 输入
-    scanf("%lf %d %d %d", &price, &month, &day, &flag);
-    // 计算
-    if (flag == 1)
-    {
-        if ((month == 11) && (day == 11))
-        {
-            price = price * 0.7 - 50;
-        }
-        if ((month == 12) && (day == 12))
-        {
-            price = price * 0.8 - 50;
-        }
-    }
-    if (flag == 0)
-    {
-        if ((month == 11) && (day == 11))
-        {
-            price = price * 0.7;
-        }
-        if ((month == 12) && (day == 12))
-        {
-            price = price * 0.8;
-        }
-    }
-    // 输出
-    if (price <= 0)
-    {
-        printf("%.2lf", price = 0);
-    }
-    else
-    {
-        printf("%.2lf", price);
-    }
+// int main()
+// {
+//     double price = 0.0;
+//     int month = 0;
+//     int day = 0;
+//     int flag = 0;
+//     // 输入
+//     scanf("%lf %d %d %d", &price, &month, &day, &flag);
+//     // 计算
+//     if (flag == 1)
+//     {
+//         if ((month == 11) && (day == 11))
+//         {
+//             price = price * 0.7 - 50;
+//         }
+//         if ((month == 12) && (day == 12))
+//         {
+//             price = price * 0.8 - 50;
+//         }
+//     }
+//     if (flag == 0)
+//     {
+//         if ((month == 11) && (day == 11))
+//         {
+//             price = price * 0.7;
+//         }
+//         if ((month == 12) && (day == 12))
+//         {
+//             price = price * 0.8;
+//         }
+//     }
+//     // 输出
+//     if (price <= 0)
+//     {
+//         printf("%.2lf", price = 0);
+//     }
+//     else
+//     {
+//         printf("%.2lf", price);
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+
+// #include <stdio.h>
+// #include <math.h>
+
+// int main()
+// {
+//     float a = 0.0;
+//     char b = 0;
+//     int fee = 0;
+//     scanf("%f %c", &a, &b);
+//     if (a < 1)
+//     {
+//         fee = 20;
+//     }
+//     else
+//     {
+//         fee = 20 + ceil(a - 1);
+//     }
+
+//     if (b == 'y')
+//     {
+//         fee += 5;
+//     }
+//     printf("%d", fee);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     float price = 0.0;
+//     scanf("%f", &price);
+//     if (price >= 100 && price < 500)
+//     {
+//         price *= 0.9;
+//     }
+//     if (price >= 500 && price < 2000)
+//     {
+//         price *= 0.8;
+//     }
+//     if (price >= 2000 && price < 5000)
+//     {
+//         price *= 0.7;
+//     }
+//     if (price >= 5000)
+//     {
+//         price *= 0.6;
+//     }
+//     printf("%.1f", price);
+
+//     return 0;
+// }
+
