5692ca03b0e1ac83ee03471d65b371d9016975c2
Weizzzzzzz <2141618527@qq.com>
2023-07-25 18:50:52 +0800

    23_7_25

--- test_7_24/test.c
+++ test_7_24/test.c
@@ -30,27 +30,244 @@
 //     return 0;
 // }
 
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n = 0;
+//     int odd = 0;
+//     int even = 0;
+//     scanf("%d",&n);
+//     int i = 0;
+//     for (i = 1; i <= n; i++)
+//     {
+//         if (i % 2 == 0)
+//         {
+//             even++;
+//         }
+//         else
+//         {
+//             odd++;
+//         }
+//     }
+//     printf("%d %d", odd, even);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int count = 0;
+//     int i = 0;
+//     int j = 0;
+//     for (i = 100; i <= 999; i++)
+//     {
+//         int flag = 1;
+//         for (j = 2; j < i; j++)
+//         {
+//             if (i % j == 0)
+//             {
+//                 flag = 0;
+//                 break;
+//             }
+//         }
+//         if (flag)
+//         {
+//             count++;
+//         }
+//     }
+//     printf("%d", count);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 0;
+//     int j = 0;
+//     for (i = 1; i < 10; i++)
+//     {
+//         for (j = 1; j <= i; j++)
+//         {
+//             printf("%d*%d=%2d ", j, i, i * j);
+//         }
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n = 0;
+//     int ret = 0;
+//     scanf("%d", &n);
+//     int i = 0;
+//     for (i = 1; i <= n; i += 2)
+//     {
+//         ret += i;
+//     }
+//     for (i = 2; i <= n; i += 2)
+//     {
+//         ret -= i;
+//     }
+//     printf("%d", ret);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n = 0;
+//     double res = 0.0f;
+//     scanf("%d", &n);
+//     int i = 0;
+//     for (i = 1; i <= n; i++)
+//     {
+//         res += (1.0 / i);
+//     }
+//     printf("%lf", res);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n = 0;
+//     int sum = 0;
+//     double res = 0.0f;
+//     int flag = 1;
+//     scanf("%d", &n);
+//     int i = 0;
+//     for (i = 1; i <= n; i++)
+//     {
+//         sum += flag * (2 * i - 1);
+//         res += 1.0 / sum;
+//         flag = -flag;
+//     }
+//     printf("%.3lf\n", res);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n = 0;
+//     int sum = 0;
+//     scanf("%d", &n);
+//     int i = 0;
+//     for (i = 1; i <= n; i++)
+//     {
+//         int j = 0;
+//         for (j = 1; j <= i; j++)
+//         {
+//             sum += j;
+//         }
+//     }
+//     printf("%d", sum);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n = 0;
+//     int res = 0;
+//     scanf("%d", &n);
+//     while (n)
+//     {
+//         res += n % 10;
+//         n /= 10;
+//     }
+//     printf("%d", res);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n = 0;
+//     int count = 0;
+//     scanf("%d", &n);
+//     while (n != 1)
+//     {
+//         if (n % 2 == 0)
+//         {
+//             n /= 2;
+//             count++;
+//         }
+//         else
+//         {
+//             n = n * 3 + 1;
+//             count++;
+//         }
+//     }
+//     printf("%d", count);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int count = 0;
+//     int num = 0;
+//     int i = 0;
+//     for (i = 1; i <= 2019; i++)
+//     {
+//         num = i;
+//         while (num)
+//         {
+//             if (num % 10 == 9)
+//             {
+//                 count++;
+//                 break;
+//             }
+//             num /= 10;
+//         }
+//     }
+//     printf("%d", count);
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
 {
     int n = 0;
-    int odd = 0;
-    int even = 0;
-    scanf("%d",&n);
-    int i = 0;
-    for (i = 1; i <= n; i++)
+    scanf("%d", &n);
+    int chinese = 0;
+    int math = 0;
+    int english = 0;
+    int average = 0;
+    int count = 0;
+    while (scanf("%d %d %d", &chinese, &math, &english) == 3)
     {
-        if (i % 2 == 0)
+        average = (chinese + math + english) / 3;
+        if (average < 60)
         {
-            even++;
-        }
-        else
-        {
-            odd++;
+            count++;
         }
     }
-    printf("%d %d", odd, even);
+    printf("%d", count);
 
     return 0;
 }
