47053b8a4f71d5d9d71a48de948872f9ec0076fd
Weizzzzzzz <2141618527@qq.com>
2023-08-02 19:50:18 +0800

    23_8_2

--- test_8_1/test.c
+++ test_8_1/test.c
@@ -75,6 +75,31 @@
 //     return 0;
 // }
 
+// #include <stdio.h>
+
+// int main()
+// {
+//     int side = 0;
+//     while (scanf("%d", &side) == 1)
+//     {
+//         for (int i = 0; i < side; i++)
+//         {
+//             int j = 0;
+//             for (j = 0; j < side - 1 - i; j++)
+//             {
+//                 printf("  ");
+//             }
+//             for (; j < side; j++)
+//             {
+//                 printf("* ");
+//             }
+//             printf("\n");
+//         }
+//     }
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
@@ -87,9 +112,9 @@ int main()
             int j = 0;
             for (j = 0; j < side - 1 - i; j++)
             {
-                printf("  ");
+                printf(" ");
             }
-            for (; j < side; j++)
+            for (j = 0; j <= i; j++)
             {
                 printf("* ");
             }
