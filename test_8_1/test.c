f36f242a3fae2129c939ee2a18abd9e2e7e055f2
Weizzzzzzz <2141618527@qq.com>
2023-08-04 20:50:47 +0800

    23_8_4

--- test_8_1/test.c
+++ test_8_1/test.c
@@ -151,6 +151,170 @@
 //     return 0;
 // }
 
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n = 0;
+//     while (scanf("%d", &n) == 1)
+//     {
+//         int i = 0;
+//         int j = 0;
+//         // 上面
+//         for (i = 0; i < n + 1; i++)
+//         {
+//             for (j = 0; j < n - i; j++)
+//             {
+//                 printf(" ");
+//             }
+//             for (j = 0; j < i + 1; j++)
+//             {
+//                 printf("* ");
+//             }
+//             printf("\n");
+//         }
+//         // 下面
+//         for (i = 0; i < n; i++)
+//         {
+//             for (j = 0; j < i + 1; j++)
+//             {
+//                 printf(" ");
+//             }
+//             for (j = 0; j < n - i; j++)
+//             {
+//                 printf("* ");
+//             }
+//             printf("\n");
+//         }
+//     }
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n = 0;
+//     while (scanf("%d", &n) == 1)
+//     {
+//         int i = 0;
+//         int j = 0;
+//         // 上面
+//         for (i = 0; i < n; i++)
+//         {
+//             for (j = 0; j < n + 1 - i; j++)
+//             {
+//                 printf("* ");
+//             }
+//             printf("\n");
+//         }
+//         // 下面
+//         for (i = 0; i < n + 1; i++)
+//         {
+//             for (j = 0; j < i + 1; j++)
+//             {
+//                 printf("* ");
+//             }
+//             printf("\n");
+//         }
+//     }
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n = 0;
+//     while (scanf("%d", &n) == 1)
+//     {
+//         int i = 0;
+//         int j = 0;
+//         // 上面
+//         for (i = 0; i < n; i++)
+//         {
+//             for (j = 0; j < n - i; j++)
+//             {
+//                 printf("  ");
+//             }
+//             for (j = 0; j < i + 1; j++)
+//             {
+//                 printf("*");
+//             }
+//             printf("\n");
+//         }
+//         // 下面
+//         for (i = 0; i < n + 1; i++)
+//         {
+//             for (j = 0; j < i; j++)
+//             {
+//                 printf("  ");
+//             }
+//             for (j = 0; j < n + 1 - i; j++)
+//             {
+//                 printf("*");
+//             }
+//             printf("\n");
+//         }
+//     }
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n = 0;
+//     while (scanf("%d", &n) == 1)
+//     {
+//         int i = 0;
+//         int j = 0;
+//         for (i = 0; i < n; i++)
+//         {
+//             // 打印空格
+//             for (j = 0; j < i; j++)
+//             {
+//                 printf(" ");
+//             }
+//             // 打印*号
+//             printf("*");
+//             // 换行
+//             printf("\n");
+//         }
+//     }
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n = 0;
+//     while (scanf("%d", &n) == 1)
+//     {
+//         int i = 0;
+//         int j = 0;
+//         for (i = 0; i < n; i++)
+//         {
+//             // 打印空格
+//             for (j = 0; j < n - 1 - i; j++)
+//             {
+//                 printf(" ");
+//             }
+//             // 打印*号
+//             printf("*");
+//             // 换行
+//             printf("\n");
+//         }
+//     }
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
@@ -160,29 +324,18 @@ int main()
     {
         int i = 0;
         int j = 0;
-        // 上面
-        for (i = 0; i < n + 1; i++)
-        {
-            for (j = 0; j < n - i; j++)
-            {
-                printf(" ");
-            }
-            for (j = 0; j < i + 1; j++)
-            {
-                printf("* ");
-            }
-            printf("\n");
-        }
-        // 下面
         for (i = 0; i < n; i++)
         {
-            for (j = 0; j < i + 1; j++)
+            for (j = 0; j < n; j++)
             {
-                printf(" ");
-            }
-            for (j = 0; j < n - i; j++)
-            {
-                printf("* ");
+                if (i == j || i + j == n - 1)
+                {
+                    printf("*");
+                }
+                else
+                {
+                    printf(" ");
+                }
             }
             printf("\n");
         }
